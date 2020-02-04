5c20876f8addc6347e7639149a3a67fe408279dd
Esteban Pintos <epintos7@gmail.com>
2020-02-04 21:48:18 +1030

    Adds solution array and initializer

--- src/classes/test.class.ts
+++ src/classes/test.class.ts
@@ -1,20 +1,38 @@
+const MAX_LENGTH = 182;
+
 export class Test {
-    private n: number
-    private m: number
-    private data: number[][]
+    readonly n: number
+    readonly m: number
+    readonly data: number[][]
+    readonly solution: number[][]
   
     public constructor(n: number, m: number) {
       this.n = n;
       this.m = m;
       this.data = [];
+      this.solution = [];
     }
   
-    isComplete(): boolean {
+    public isComplete(): boolean {
         return this.n === this.data.length;
     }
 
-    addColumn(column: number[]) {
+    public addColumn(column: number[]) {
         // TODO: Raise error if column.length != this.m
         this.data.push(column);
+    }
+
+    public initializeSolution(): void {
+      for (let i = 0; i < this.n; i++) {
+        for (let j = 0; j < this.m; j++) {
+          this.solution[i][j] = MAX_LENGTH * MAX_LENGTH;
+        }
+      }
+    }
+
+    public printSolution() {
+      this.solution.forEach(element => {
+        console.log(element);
+      });
     }
   }
---
+++ src/classes/testsHandler.class.ts
@@ -0,0 +1,48 @@
+import { Test } from './index';
+
+export class TestsHandler {
+    _testsNumber: number | undefined
+    readonly tests: Test[]
+
+    public constructor() {
+      this.tests = [];
+    }
+
+    public addTest(n: number, m: number): void {
+      this.tests.push(new Test(n, m));
+    }
+
+    public testsComplete(): boolean {
+        return this.testsNumber === this.tests.length && this.tests.every(test => test.isComplete());
+    }
+
+    get testsNumber(): number | undefined {
+      return this._testsNumber;
+    }
+
+    set testsNumber(value: number | undefined) {
+      this._testsNumber = value;
+    }
+
+    public isLastComplete(): boolean {
+        const lastTest = this.lastTest();
+        if (lastTest === undefined) {
+          return true;
+        } else {
+          return lastTest.isComplete();
+        }
+        
+    }
+
+    public lastTest(): Test | undefined {
+      if (this.tests.length === 0) {
+        return undefined;
+      } else {
+        return this.tests[this.tests.length - 1];
+      }
+    }
+
+    public initializeSolutions(): void {
+      this.tests.forEach(test => test.initializeSolution());
+    }
+}
